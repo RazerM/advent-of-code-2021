ba3b66d66acad86d4f6d7d1a733b9e8446fe6090
Frazer McLean <frazer@frazermclean.co.uk>
2021-12-13 02:00:08 +0000

    day 7

--- src/aoc2021/day07.py
+++ src/aoc2021/day07.py
@@ -1,3 +1,4 @@
+import math
 from typing import IO
 
 from ._registry import register
@@ -5,4 +6,14 @@ from ._registry import register
 
 @register(day=7)
 def solve(file: IO[str], verbose: int) -> None:
-    pass
+    positions = list(map(int, file.read().strip().split(",")))
+    human_engineering = dict()
+    crab_engineering = dict()
+    for value in range(min(positions), max(positions) + 1):
+        human_engineering[value] = sum(abs(p - value) for p in positions)
+        crab_engineering[value] = sum(
+            math.comb(abs(p - value) + 1, 2) for p in positions
+        )
+
+    print("Part 1:", min(human_engineering.values()))
+    print("Part 2:", min(crab_engineering.values()))
